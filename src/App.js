d29bacacac704a2d96c01985a404505646c13135
MonWas <65023903+MonWas@users.noreply.github.com>
2021-03-04 10:55:59 +0100

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 export default function App() {
   return (
   <div className="App">
+    <div className="container">
     <h1>Weather App</h1>
     <footer>
-      This project was coded by Monique Wassermann and is open-sourced on GitHub
+      This project was coded by Monique Wassermann and is <a href="https://github.com/MonWas/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
     </footer>
+    </div>
   </div>
   );
 }
